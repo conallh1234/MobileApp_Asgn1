84c8cd7406fb4f75641e725ba9b09bd9ae4f7457
conallh1234 <conallh1234@hotmail.com>
2021-10-15 18:49:33 +0100

    "Event Manager V0.1.3 - Added menu() function to EventView"

--- src/main/kotlin/org/wit/eventmngr/console/views/EventView.kt
+++ src/main/kotlin/org/wit/eventmngr/console/views/EventView.kt
@@ -1,4 +1,27 @@
 package org.wit.eventmngr.console.views
 
 class EventView {
+
+
+    fun menu() : Int {
+        var option : Int
+        var input : String?
+
+        println("EVENT MANAGER SYSTEM - MAIN MENU")
+        println(" 1. Add An Event")
+        println(" 2. Update an Event's Details")
+        println(" 3. List All Stored Events")
+        println(" 4. Search Stored Events")
+        println(" 5. Delete An Event")
+        println(" -1. Exit App")
+        println()
+        print("Enter Option: ")
+        input = readLine()!!
+        option = if(input.toIntOrNull() != null && !input.isEmpty())
+            input.toInt()
+        else
+            -9
+        return option
+    }
+
 }

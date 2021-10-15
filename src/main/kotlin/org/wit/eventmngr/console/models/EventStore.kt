08a5868b0a132186ddec5f51373f4c5baf4fc48f
conallh1234 <conallh1234@hotmail.com>
2021-10-15 18:40:04 +0100

    "Event Manager V0.1.2 - Adding EventStore Details"

--- src/main/kotlin/org/wit/eventmngr/console/models/EventStore.kt
+++ src/main/kotlin/org/wit/eventmngr/console/models/EventStore.kt
@@ -1,4 +1,9 @@
 package org.wit.eventmngr.console.models
 
 interface EventStore {
+    fun findAll(): List<EventModel>
+    fun findOne(id: Long): EventModel?
+    fun create(event: EventModel)
+    fun update(event: EventModel)
+    fun delete(event: EventModel)
 }

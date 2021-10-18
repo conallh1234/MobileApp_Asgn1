58165a1d47eda4a0ce7955ebc079ae46ec361da3
conallh1234 <conallh1234@hotmail.com>
2021-10-18 12:51:46 +0100

    "Event Manager V0.1.8 - Initial Structure of EventView Developed"

--- src/main/kotlin/org/wit/eventmngr/console/views/EventView.kt
+++ src/main/kotlin/org/wit/eventmngr/console/views/EventView.kt
@@ -1,7 +1,10 @@
 package org.wit.eventmngr.console.views
 
-class EventView {
+import org.wit.eventmngr.console.models.EventJSONStore
+//import org.wit.eventmngr.console.models.EventMemStore
+import org.wit.eventmngr.console.models.EventModel
 
+class EventView {
 
     fun menu() : Int {
         var option : Int
@@ -9,10 +12,11 @@ class EventView {
 
         println("EVENT MANAGER SYSTEM - MAIN MENU")
         println(" 1. Add An Event")
-        println(" 2. Update an Event's Details")
-        println(" 3. List All Stored Events")
-        println(" 4. Search Stored Events")
-        println(" 5. Delete An Event")
+        println(" 2. Update an Event's Title & Description")
+        println(" 3. Update an Events's Attendees")
+        println(" 4. List All Stored Events")
+        println(" 5. Search Stored Events")
+        println(" 6. Delete An Event")
         println(" -1. Exit App")
         println()
         print("Enter Option: ")
@@ -24,4 +28,65 @@ class EventView {
         return option
     }
 
+    fun listEvents(events : EventJSONStore) {
+        println("List All Events")
+        println()
+        events.logAll()
+        println()
+    }
+
+    fun showEvent(event : EventModel) {
+        if(event != null)
+            println("Event Details [ $event ]")
+        else
+            println("Event Not Found...")
+    }
+
+    fun addEventData(event: EventModel) : Boolean {
+
+        println()
+        print("Enter a Title : ")
+        event.title = readLine()!!
+        print("Enter a Description : ")
+        event.description = readLine()!!
+
+        return event.title.isNotEmpty() && event.description.isNotEmpty()
+    }
+
+    fun updateEventDetails(event: EventModel) : Boolean {
+
+        var tempTitle: String?
+        var tempDescription: String?
+
+        if (event != null) {
+            print("Enter a new Title for [ " + event.title + " ] : ")
+            tempTitle = readLine()!!
+            print("Enter a new Description for [ " + event.description + " ] : ")
+            tempDescription = readLine()!!
+
+            if (!tempTitle.isNullOrEmpty() && !tempDescription.isNullOrEmpty()) {
+                event.title = tempTitle
+                event.description = tempDescription
+                return true
+            }
+        }
+        return false
+    }
+
+    fun updateEventAttendees(event: EventModel) : Boolean {
+        return false
+    }
+
+    fun getId() : Long {
+        var strId : String? // String to hold user input
+        var searchId : Long // Long to hold converted id
+        print("Enter id to Search/Update/Delete : ")
+        strId = readLine()!!
+        searchId = if (strId.toLongOrNull() != null && !strId.isEmpty())
+            strId.toLong()
+        else
+            -9
+
+        return searchId
+    }
 }

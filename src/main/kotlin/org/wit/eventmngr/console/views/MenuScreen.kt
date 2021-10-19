f2f6a0f2b6d8fd982ab9db39ca4ec4f8c4ae76aa
conallh1234 <conallh1234@hotmail.com>
2021-10-19 14:35:46 +0100

    "Event Manager V0.2.7 - Initial Menu Screen Structure"

--- src/main/kotlin/org/wit/eventmngr/console/views/MenuScreen.kt
+++ src/main/kotlin/org/wit/eventmngr/console/views/MenuScreen.kt
@@ -1,4 +1,52 @@
 package org.wit.eventmngr.console.views
 
-class MenuScreen {
-}
+import javafx.application.Platform
+import javafx.geometry.Orientation
+import org.wit.eventmngr.console.controllers.EventUIController
+import tornadofx.*
+
+
+class MenuScreen : View("Placemark Main Menu") {
+
+    val placemarkUIController: PlacemarkUIController by inject()
+
+    override val root = form {
+        setPrefSize(400.0, 200.0)
+        fieldset(labelPosition = Orientation.VERTICAL) {
+            text("")
+            button("Add Placemark") {
+
+                isDefaultButton = true
+                useMaxWidth = true
+                action {
+                    runAsyncWithProgress {
+                        placemarkUIController.loadAddScreen()
+                    }
+                }
+            }
+            text("")
+            button("List Placemarks") {
+
+                isDefaultButton = true
+                useMaxWidth = true
+                action {
+                    runAsyncWithProgress {
+                        placemarkUIController.loadListScreen()
+                    }
+                }
+            }
+            text("")
+            button("Exit") {
+
+                isDefaultButton = true
+                useMaxWidth = true
+                action {
+                    runAsyncWithProgress {
+                        Platform.exit();
+                        System.exit(0);
+                    }
+                }
+            }
+        }
+
+    }
